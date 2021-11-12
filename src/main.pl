f80282fd053209b3765ee390fc282537e0f63a16
febryola <febryola0402@gmail.com>
2021-11-13 02:35:50 +0700

    m

---
+++ src/main.pl
@@ -0,0 +1,57 @@
+:- dynamic(runProgram/1).
+
+/* include file yang dibutuhkan untuk main */
+:- include('map.pl').
+
+%memulai permainan
+startGame :-
+    runProgram(_),
+    write('You have started the game, have fun!'),!.
+
+startGame :-
+    \+runProgram(_),    
+    write('\33\[38;5;96m██╗░░██╗░█████╗░██████╗░██╗░░░██╗███████╗░██████╗████████╗  ██╗░░██╗██████╗░██╗░░░██╗░██████╗░░█████╗░██████╗░\33\[0m'),nl,
+    write('\33\[38;5;96m██║░░██║██╔══██╗██╔══██╗██║░░░██║██╔════╝██╔════╝╚══██╔══╝  ██║░░██║██╔══██╗╚██╗░██╔╝██╔════╝░██╔══██╗╚════██╗\33\[0m'),nl,
+    write('\33\[38;5;96m███████║███████║██████╔╝╚██╗░██╔╝█████╗░░╚█████╗░░░░██║░░░  ███████║██████╦╝░╚████╔╝░██║░░██╗░██║░░██║░█████╔╝\33\[0m'),nl,
+    write('\33\[38;5;96m██╔══██║██╔══██║██╔══██╗░╚████╔╝░██╔══╝░░░╚═══██╗░░░██║░░░  ██╔══██║██╔══██╗░░╚██╔╝░░██║░░╚██╗██║░░██║░╚═══██╗\33\[0m'),nl,
+    write('\33\[38;5;96m██║░░██║██║░░██║██║░░██║░░╚██╔╝░░███████╗██████╔╝░░░██║░░░  ██║░░██║██████╦╝░░░██║░░░╚██████╔╝╚█████╔╝██████╔╝\33\[0m'),nl,
+    write('\33\[38;5;96m╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═════╝░░░░╚═╝░░░  ╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░░╚═════╝░░╚════╝░╚═════╝░\33\[0m'),nl,                                                          
+    nl,
+    asserta(quest(0,0,0,0,0)),
+    repeat,
+    write('Welcome to Harvest HBYG03, this game will make your day better!!!'),nl,
+    write('************* Enjoy and be the winner of this game *************!'),nl,
+    write('\33\[38;5;91m1. New Game.\33\[0m'),nl,
+    write('\33\[38;5;96m2. Load Game.\33\[0m'),nl,nl,
+    write('Insert number : '),read(X),
+    (X =:= 1 -> newGame ;X =:= 2 -> loadGame),
+
+    asserta(runProgram(1)),
+
+    write('\nplese type \'help.\' to find out what commands can be used in this game!!!'),!.
+
+% new game
+newGame :- runProgram(_), write('The game has started!'),!.
+
+newGame:- write('let me know, who are you? (tulis di antara tanda petik dan diakhiri titik. Contoh: \'hbyg\'.)'),nl,
+          write('Your name is '), read(Username),
+          asserta(nama(Username)),nl,
+          write('Hi '), write(Username),write('! Enjoy and be the winner of this game!!!.'),nl,nl,
+          asserta(runProgram(1)),
+          asserta(chapter(0)),
+          /* TODO : inisialisasi role player dulu*/
+          /* TODO : inisialisasi hal -hal lain yang dibutuhkan*/
+          initMap,
+          !.
+% menampilkan command yang dapat digunakan
+help :- runProgram(_),
+        write('\33\[38;5;93mCommand: \33\[0m'), nl,
+        write('\33\[38;5;31m    startGame.          \33\[0m: memulai permainan'), nl,
+        write('\33\[38;5;31m    quitGame.           \33\[0m: keluar dari permainan'), nl,
+        write('\33\[38;5;31m    help.               \33\[0m: melihat command apa saja yang dapat digunakan'), nl,
+        write('\33\[38;5;31m    map.                \33\[0m: melihat peta game'),nl,
+        write('\33\[38;5;31m    saveGame            \33\[0m: melakukan penyimpanan game'), nl,
+      	write('\33\[38;5;31m    status.             \33\[0m: cek status player'), nl,
+        write('\33\[38;5;31m    quest.              \33\[0m: cek progress quest'), nl,
+        write('\33\[38;5;31m    cekInventory.       \33\[0m: cek inventory'), nl,!.
+/*sementara itu dulu buat command help*/
---
+++ src/map.pl
@@ -0,0 +1,150 @@
+:- dynamic(width/1).
+:- dynamic(length/1).
+:- dynamic(runProgram/1).
+
+/* Menginisialisasi Map */
+initMap:-
+    asserta(length(20)),
+    asserta(width(20)),
+    length(P),
+    width(L),
+    initPlayer.
+
+/* Menginisialisasi Posisi Awal Player */
+initPlayer :-
+    asserta(posisi(1,1)).
+
+/* Mengecek apakah kooordinat X,Y posisi Player */
+isPlayer(X,Y) :-
+    posisi(A,B),
+    X =:= A,
+    Y =:= B.
+
+/* Mengecek apakah kooordinat X,Y posisi Quest */
+isQuest(X,Y) :-
+    X =:= 8,
+    Y =:= 7.
+
+/* Mengecek apakah kooordinat X,Y posisi Market */
+isMarket(X,Y) :-
+    X =:= 15,
+    Y =:= 16.
+
+/* Mengecek apakah kooordinat X,Y posisi House */
+isHouse(X,Y) :-
+    X =:= 16,
+    Y =:= 4.
+
+/* Mengecek apakah kooordinat X,Y posisi Ranch */
+isRanch(X,Y) :-
+    X =:= 3,
+    Y =:= 17.
+
+/* Mengecek apakah kooordinat X,Y posisi Border atau Ujung Map */
+isBorder(X,_) :-
+    X =:= 0.
+isBorder(X,_) :-
+    length(P),
+    X =:= P.
+isBorder(_,Y) :-
+    Y =:= 0.
+isBorder(_,Y) :-
+    width(L),
+    Y =:= L.
+
+/* menampilkan air atau lokasi untuk memancing*/
+isWater(X,Y) :-
+    X =:= 9,
+    Y =:= 10;
+    X =:= 10,
+    Y =:= 10;
+    X =:= 11,
+    Y =:= 10;
+    X =:= 8,
+    Y =:= 11;
+    X =:= 9,
+    Y =:= 11;
+    X =:= 10,
+    Y =:= 11;
+    X =:= 11,
+    Y =:= 11;
+    X =:= 12,
+    Y =:= 11;
+    X =:= 9,
+    Y =:= 12;
+    X =:= 10,
+    Y =:= 12;
+    X =:= 11,
+    Y =:= 12.
+
+/* Menampilkan lokasi setiap tempat pada map */
+cetakMap(X,Y) :-
+    length(P),
+    width(L),
+    isBorder(X,Y),
+    X =:= P,
+    Y =:= L,
+    write('🗿').
+
+cetakMap(X,Y) :-
+    length(P),
+    isBorder(X,Y),
+    X =:= P,
+    write('🗿'),nl,
+    NextX is 0,
+    NextY is Y+1,
+    cetakMap(NextX,NextY).
+
+cetakMap(X,Y) :-
+    isBorder(X,Y),
+    write('🗿'),
+    NextX is X+1,
+    cetakMap(NextX,Y).
+
+/* api artinya quest untuk setiap misi*/
+cetakMap(X,Y) :-
+    isQuest(X,Y),
+    write('🔥'), 
+    NextX is (X+1),
+    cetakMap(NextX,Y).
+
+cetakMap(X,Y) :-
+    isMarket(X,Y),
+    write('🛒'),
+    NextX is (X+1),
+    cetakMap(NextX,Y).
+
+cetakMap(X,Y) :-
+    isHouse(X,Y),
+    write('🏠'),
+    NextX is (X+1),
+    cetakMap(NextX,Y).
+
+cetakMap(X,Y) :-
+    isRanch(X,Y),
+    write('🐄'),
+    NextX is (X+1),
+    cetakMap(NextX,Y).
+
+cetakMap(X,Y) :-
+    isPlayer(X,Y),
+    write('💂'),
+    NextX is (X+1),
+    cetakMap(NextX,Y).
+
+cetakMap(X,Y) :-
+    isWater(X,Y),
+    write('🌊'),
+    NextX is (X+1),
+    cetakMap(NextX,Y).
+
+cetakMap(X,Y) :-
+    \+isBorder(X,Y),
+    write(' -'),
+    NextX is (X+1),
+    cetakMap(NextX,Y).
+
+/* Menampilkan Map */
+map :-
+    runProgram(_),
+    cetakMap(0,0),!.
