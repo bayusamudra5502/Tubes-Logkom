1f2166255e9f8cd15d9670fbdc8ac02398362b87
Febryola Kurnia Putri <71137410+febryola@users.noreply.github.com>
2021-11-16 22:41:07 +0700

    update status player

--- src/player.pl
+++ src/player.pl
@@ -1,7 +1,7 @@
-:- dynamic(player/12).
+:- dynamic(player/13).
 
-% player(Job, level, levelFarming, expFarming, levelFishing, expFishing, levelRanching, expRanching, exp, base exp, gold, energi tersisa)
-% player(A,B,C,D,E,F,G,H, I, J, K,L)
+% player(Job, level, levelFarming, expFarming, base expFarming, levelFishing, expFishing, base expFishing, levelRanching, expRanching, base expRanching, exp, base exp)
+% player(A,B,C,D,E,F,G,H, I, J, K,L,M)
 
 initPemain :- 
             write('\33\[38;5;220mHii, before start the game, Please Choose Your Job\33\[0m'),nl,
@@ -9,41 +9,42 @@ initPemain :-
             write('2. Farmer.'),nl,
             write('3. Rancher.'),nl,
             write('Masukkan angka : '), read(X),nl,
-            (X =:= 1 ->asserta(player('Fisherman', 1,1,56,1,76,1,56,0,250,100,50)) ;
-            X =:= 2 ->asserta(player('Farmer', 1,1,56,1,76,1,56,0,250,100,50));
-            X =:= 3 ->asserta(player('Rancher', 1,1,56,1,76,1,56,0,250,100,50))).
+            (X =:= 1 ->asserta(player('Fisherman', 1,1,56,150,1,76,150,1,56,150,0,250)) ;
+            X =:= 2 ->asserta(player('Farmer', 1,1,56,150,1,76,150,1,56,150,0,250));
+            X =:= 3 ->asserta(player('Rancher', 1,1,56,150,1,76,150,1,56,150,0,250))).
 
 
-levelUp :- player(A,B,C,D,E,F,G,H,I,J,K,L), I >= J,
+levelUp :- player(A,B,C,D,E,F,G,H,I,J,K,L,M), I >= J,
           Bnew is B+1,
-          Cnew is C,
-          Dnew is D,
-          Enew is E,
-          Fnew is F,
-          Gnew is G,
-          Hnew is H,
-          Inew is I mod J,
-          Jnew is J*2,
-          Knew is K,
-          Lnew is L,
-          retract(player(A,B,C,D,E,F,G,H,I,J,K,L)), asserta(player(A,Bnew,Cnew,Dnew,Enew,Fnew,Gnew,Hnew,Inew,Jnew,Knew,Lnew)),
+          Cnew is C+1,
+          Dnew is D mod E,
+          Enew is E*2,
+          Fnew is F+1,
+          Gnew is G mod H,
+          Hnew is H*2,
+          Inew is I+1,
+          Jnew is J mod K,
+          Knew is K*2,
+          Lnew is L mod M,
+          Mnew is M*2,
+          retract(player(A,B,C,D,E,F,G,H,I,J,K,L)), asserta(player(A,Bnew,Cnew,Dnew,Enew,Fnew,Gnew,Hnew,Inew,Jnew,Knew,Lnew,Mnew)),
           write('Level up from '), write(B), write(' -> '),write(Bnew),nl,!.
 levelUp :-!.
 /* TODO : level up untuk masing2 parameter ranch,farmer, fish, sesuai exp dan levelnya */
 
-status :- player(A,B,C,D,E,F,G,H,I,J,K,L),
+status :- player(A,B,C,D,E,F,G,H,I,J,K,L,M),gold(Gold),energi(Energi),
 	      write('\33\[38;5;76m####################################################\33\[0m'),nl,
 	      write('\33\[38;5;76m#                   \33\[0m STATUS PEMAIN                 \33\[38;5;76m#\33\[0m'),nl,
 	      write('\33\[38;5;76m####################################################\33\[0m'),nl,
           write('Job           : '), write(A),nl,
           write('Level         : '), write(B),nl,
           write('Level Farming : '), write(C),nl,
-          write('Exp Farming   : '), write(D),nl,
-          write('Level Fishing : '), write(E),nl,
-          write('Exp Fishing   : '), write(F),nl,
-          write('Level Ranching: '), write(G),nl,
-          write('Exp Ranching  : '), write(H),nl,
-          write('Exp           : '), write(I),write('/'),write(J),nl,
-          write('Gold          : '), write(K),nl,
-          write('Energi Tersisa: '), write(L),nl,!.
+          write('Exp Farming   : '), write(D),write('/'),write(E),nl,
+          write('Level Fishing : '), write(F),nl,
+          write('Exp Fishing   : '), write(G),write('/'),write(H),nl,
+          write('Level Ranching: '), write(I),nl,
+          write('Exp Ranching  : '), write(J),write('/'),write(K),nl,
+          write('Exp           : '), write(L),write('/'),write(M),nl,
+          write('Gold          : '), write(Gold),nl,
+          write('Energi Tersisa: '), write(Energi),nl,!.
 
