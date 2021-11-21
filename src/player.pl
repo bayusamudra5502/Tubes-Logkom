bc9c496b104c841ab2b868a0b5194b7e945b4b85
Aloysius Gilang <gilangpramdya84@gmail.com>
2021-11-21 15:12:03 +0700

    add inventory system : insert

--- src/player.pl
+++ src/player.pl
@@ -1,7 +1,7 @@
 :- dynamic(player/13).
-:- dynamic(inventory/2).
+:- dynamic(inventory/1).
 
-% inventory ([list], length)
+% inventory ([list])
 % list elemen : ID, amount
 
 
@@ -278,3 +278,52 @@ status :- player(A,B,C,D,E,F,G,H,I,J,K,L,M),gold(Gold),energi(Energi),
           write('Gold          : '), write(Gold),nl,
           write('Energi Tersisa: '), write(Energi),nl,!.
 
+/* Management Inventory */
+inventory([]).
+
+% insert_last( elemen, list, result)
+insert_last(X, [], [X]) :- !.
+insert_last(X, [H|T1], [H|T2]):- insert_last(X,T1, T2).
+
+% delete_at_n( n, list, result)
+delete_at_n(1, [_|T], T) :- !.
+delete_at_n(N, [H|T1], [H|T2]) :- N1 is N-1, delete_at_n(N1, T1, T2).
+
+% select_nth (list, n, result)
+select_nth([H|_],0, H) :- !.
+select_nth([_|T],N, X) :- N1 is N-1, select_nth(T, N1, X).
+
+% set_nth(list, n, newval, updatedlist)
+set_nth([_|T], 0, X, [X|T]).
+set_nth([H|T1], N, X, [H|T2]):-N1 is N-1, set_nth(T1, N1, X, T2).
+
+% cek apakah item dengan id ID atau idx Index sudah ada di inventory
+% is_member(ID,list,index)
+head([H|_], H).
+is_member(X, [Y|_],0) :- head(Y,X),!.
+is_member(X, [_|T],Index) :- is_member(X,T,Index1), Index is Index1 + 1.
+
+% inv_len (list, length)
+inv_len([],0).
+inv_len([_|T],L) :- inv_len(T,LT), L is LT + 1.
+
+% menambah item yang belum ada ke inventory
+insert_item(ID, Amount) :- inventory(CurrentInventory), 
+    \+is_member(ID,CurrentInventory,_),
+    insert_last([ID,Amount],CurrentInventory,NewInventory),
+    assertz(inventory(NewInventory)),
+    retract(inventory(CurrentInventory)), !.
+
+% menambah item yang sudah ada ke inventory
+insert_item(ID, Amount) :- inventory(CurrentInventory), 
+    is_member(ID,CurrentInventory,Index),
+    select_nth(CurrentInventory, Index, [X,N]),
+    N1 is N + Amount,
+    set_nth(CurrentInventory, Index, [ID,N1], NewInventory),
+    assertz(inventory(NewInventory)),
+    retract(inventory(CurrentInventory)), !.
+
+print_first_item:-
+    inventory(X),
+    select_nth(X,0,Y1),
+    write(Y1),
